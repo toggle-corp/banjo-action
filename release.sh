7d758bf67bec325bc78eaf1af212e0eee29c45c5
thenav56 <ayernavin@gmail.com>
2026-03-23 13:30:40 +0545

    fix: cliff default branch

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@ export -f release_custom_hook
 export START_COMMIT=8e193ed343a53166dd0a54511ea3cd0ea07fec96
 export RELEASE_CUSTOM_HOOK=release_custom_hook
 export REPO_NAME=toggle-corp/toggle-django-action
-export DEFAULT_BRANCH=develop
+export DEFAULT_BRANCH=main
 
 export GIT_CLIFF__REMOTE__GITHUB__OWNER=toggle-corp
 export GIT_CLIFF__REMOTE__GITHUB__REPO=toggle-django-action
